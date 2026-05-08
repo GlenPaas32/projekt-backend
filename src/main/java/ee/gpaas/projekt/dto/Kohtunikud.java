bc22ca44a43302d55c92c99fdbeebed919d98c47
Glen Paas <glenpaas@tlu.ee>
2026-05-08 14:25:19 +0300

    Delete src directory

--- src/main/java/ee/gpaas/projekt/dto/Kohtunikud.java
+++
@@ -1,4 +0,0 @@
-package ee.gpaas.projekt.dto;
-
-public record Kohtunikud(String nimi, int vanus, String spordiala) {
-}
--- src/main/java/ee/gpaas/projekt/repository/DisciplineRepository.java
+++
@@ -1,9 +0,0 @@
-package ee.gpaas.projekt.repository;
-
-import ee.gpaas.projekt.entity.Discipline;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface DisciplineRepository extends JpaRepository<Discipline, Long> {
-    Optional<Discipline> findByNameIgnoreCase(String name);
-}
